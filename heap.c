30ba5192cd22baedf3012b6d2cbb1feca434087c
Richard Wild <richardwild76@gmail.com>
2018-04-01 22:43:51 +0100

    Use spaces not tabs

--- heap.c
+++ heap.c
@@ -5,7 +5,7 @@ void* allocate_array(int no_elements, size_t element_size)
 {
     long array_bytes = no_elements * element_size;
     void* array = malloc(array_bytes);
-	if (array == NULL)
+    if (array == NULL)
         error(MALLOC_FAILED);
     else
         return array;
