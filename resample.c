19440cb5ca7e826e45e1eadbb698b509b9727ea7
Richard Wild <richardwild76@gmail.com>
2018-04-01 16:45:55 +0100

    Create allocate_array function in heap.c

--- resample.c
+++ resample.c
@@ -1,8 +1,7 @@
 #include <stdlib.h>
-#include "error.h"
+#include "heap.h"
 
 #define NO_PHASE_INCREMENTS 2047
-#define PHASE_INCREMENTS_LENGTH_BYTES NO_PHASE_INCREMENTS * sizeof(long)
 
 long phase_increment(int p_period, long p_sample_rate)
 {
@@ -14,9 +13,7 @@ long phase_increment(int p_period, long p_sample_rate)
 
 long* calculate_phase_increments(long p_sample_rate)
 {
-	long* phase_increments = (long*) malloc(PHASE_INCREMENTS_LENGTH_BYTES);
-	if (phase_increments == NULL)
-		error(MALLOC_FAILED);
+	long* phase_increments = (long*) allocate_array(NO_PHASE_INCREMENTS, sizeof(long));
 
 	for (int i=0; i<NO_PHASE_INCREMENTS; i++)
 		phase_increments[i] = phase_increment(i + 1, p_sample_rate);
