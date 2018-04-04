efcd1942eac15aa37aca3f40259adcd47f1f4a3e
Richard Wild <richardwild76@gmail.com>
2018-04-04 09:18:42 +0100

    Refactor resample.c

--- resample.c
+++ resample.c
@@ -5,25 +5,20 @@
 #define PITCH_QUANTA 2047
 #define PHASE_INCREMENT_CONVERSION 3273808.59375
 
-double* phase_increments;
-unsigned char* resample_buffer;
+double *phase_increments;
+unsigned char *resample_buffer;
 
-void calculate_phase_increments(long sample_rate)
+void calculate_phase_increments(const long sample_rate)
 {
-    phase_increments = (double*) allocate_array(PITCH_QUANTA, sizeof(double));
+    phase_increments = (double *) allocate_array(PITCH_QUANTA, sizeof(double));
 
-    for (int period=1; period<=PITCH_QUANTA; period++)
-        phase_increments[period - 1] = PHASE_INCREMENT_CONVERSION/(period * sample_rate);
+    for (int period = 1; period <= PITCH_QUANTA; period++)
+        phase_increments[period - 1] = PHASE_INCREMENT_CONVERSION / (period * sample_rate);
 }
 
 void allocate_resample_buffer()
 {
-    resample_buffer = (unsigned char*) allocate_array(BUF_SIZE, sizeof(unsigned char));
-}
-
-static inline void increment_phase_accumulator(channel_info *voice)
-{
-    voice->phase_accumulator += phase_increments[voice->period];
+    resample_buffer = (unsigned char *) allocate_array(BUF_SIZE, sizeof(unsigned char));
 }
 
 static inline bool end_of_sample(const channel_info *voice)
@@ -36,14 +31,14 @@ static inline void loop_sample(channel_info *voice)
     voice->phase_accumulator -= voice->repeat_length;
 }
 
-unsigned char* resample(channel_info* voice, long frames_to_write)
+void write_audio_to_resample_buffer(channel_info *voice, const long frames_to_write)
 {
-    memset(resample_buffer, 0, BUF_SIZE);
-    unsigned char* sample = voice->sample_pointer;
+    const unsigned char *sample = voice->sample_pointer;
+    const double phase_increment = phase_increments[voice->period];
     for (long frame = 0; voice->channel_playing && frame < frames_to_write; frame++)
     {
         resample_buffer[frame] = sample[(long) voice->phase_accumulator];
-        increment_phase_accumulator(voice);
+        voice->phase_accumulator += phase_increment;
         if (end_of_sample(voice))
         {
             if (voice->sample_repeats)
@@ -52,5 +47,12 @@ unsigned char* resample(channel_info* voice, long frames_to_write)
                 voice->channel_playing = false;
         }
     }
+}
+
+unsigned char *resample(channel_info *voice, const long frames_to_write)
+{
+    memset(resample_buffer, 0, BUF_SIZE);
+    if (voice->channel_playing)
+        write_audio_to_resample_buffer(voice, frames_to_write);
     return resample_buffer;
 }
