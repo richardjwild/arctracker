0522a4426cf4eca1fcc8589ad753cf24112e681c
Richard Wild <richardwild76@gmail.com>
2018-04-04 09:20:21 +0100

    Make parameters const

--- resample.h
+++ resample.h
@@ -1,5 +1,5 @@
-long* calculate_phase_increments(long p_sample_rate);
+long* calculate_phase_increments(const long p_sample_rate);
 
 void allocate_resample_buffer();
 
-unsigned char* resample(channel_info* voice, long frames_to_write);
+unsigned char* resample(channel_info* voice, const long frames_to_write);
