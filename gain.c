8ed77e352f5a3d6bdab9c0048bc72988d0b3c858
Richard Wild <richardwild76@gmail.com>
2018-04-13 18:02:42 +0100

    Fiddle with the names

--- gain.c
+++ gain.c
@@ -4,8 +4,9 @@
 
 static int master_gain;
 
-const long pan_gain_l[] = {256, 212, 172, 128, 84, 44, 0};
-const long pan_gain_r[] = {0, 44, 84, 128, 172, 212, 256};
+const unsigned char max_logarithmic_gain = 255;
+const long pan_l[] = {256, 212, 172, 128, 84, 44, 0};
+const long pan_r[] = {0, 44, 84, 128, 172, 212, 256};
 
 void set_master_gain(int gain)
 {
@@ -15,7 +16,7 @@ void set_master_gain(int gain)
 static inline
 unsigned char adjust_logarithmic_gain(const unsigned char mlaw, const unsigned char gain)
 {
-    const unsigned char adjustment = ((unsigned char) 255) - gain;
+    const unsigned char adjustment = max_logarithmic_gain - gain;
     if (mlaw > adjustment)
         return mlaw - adjustment;
     else
@@ -26,8 +27,8 @@ stereo_frame_t apply_gain(unsigned char mu_law, channel_info *voice)
 {
     stereo_frame_t stereo_frame;
     mu_law = adjust_logarithmic_gain(mu_law, voice->gain);
-    long pcm = linear_pcm[mu_law];
-    stereo_frame.l = master_gain * pcm * pan_gain_l[voice->panning] >> 16;
-    stereo_frame.r = master_gain * pcm * pan_gain_r[voice->panning] >> 16;
+    const long pcm = linear_pcm[mu_law];
+    stereo_frame.l = master_gain * pcm * pan_l[voice->panning] >> 16;
+    stereo_frame.r = master_gain * pcm * pan_r[voice->panning] >> 16;
     return stereo_frame;
 }
