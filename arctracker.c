ab0f6c2e6ff9d75d7b5979dfc186ebaf59d24dc6
Richard Wild <richardwild76@gmail.com>
2018-05-22 13:18:53 +0100

    Replace tabs with spaces

--- arctracker.c
+++ arctracker.c
@@ -9,21 +9,21 @@ audio_api_t initialise_audio_api();
 
 int main(int argc, char *argv[])
 {
-	read_configuration(argc, argv);
+    read_configuration(argc, argv);
     module_t module = read_file();
     audio_api_t audio_api = initialise_audio_api();
-	play_module(&module, audio_api);
-	audio_api.finish();
-	exit(EXIT_SUCCESS);
+    play_module(&module, audio_api);
+    audio_api.finish();
+    exit(EXIT_SUCCESS);
 }
 
 audio_api_t initialise_audio_api()
 {
-	switch (configuration().api)
-	{
-		case OSS:
-			return initialise_oss();
-		case ALSA:
-			return initialise_alsa();
-	}
+    switch (configuration().api)
+    {
+        case OSS:
+            return initialise_oss();
+        case ALSA:
+            return initialise_alsa();
+    }
 }
