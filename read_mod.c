741f1ba256cfc8d4ce32c96e9fbdde0519af3877
Richard Wild <richardwild76@gmail.com>
2018-06-29 18:35:09 +0100

    Reorder functions in read_mod.c

--- read_mod.c
+++ read_mod.c
@@ -7,29 +7,9 @@
 #include "read_mod.h"
 #include "heap.h"
 
-size_t file_size(int fd)
-{
-    struct stat statbuf;
-    if (fstat(fd, &statbuf) == -1)
-    {
-        system_error("Error reading file status");
-    }
-    return (size_t) statbuf.st_size;
-}
+mapped_file_t load_file(char *filename);
 
-mapped_file_t load_file(char *filename)
-{
-    mapped_file_t mapped_file;
-    FILE *fp = fopen(filename, READONLY);
-    if (fp == NULL)
-    {
-        error("Cannot open file.");
-    }
-    int fd = fileno(fp);
-    mapped_file.size = file_size(fd);
-    mapped_file.addr = mmap(NULL, mapped_file.size, PROT_READ, MAP_SHARED, fd, 0);
-    return mapped_file;
-}
+size_t file_size(int fd);
 
 module_t read_file(module_format *formats, int no_formats)
 {
@@ -48,6 +28,30 @@ module_t read_file(module_format *formats, int no_formats)
     }
 
     error("File type not recognised");
+}
+
+mapped_file_t load_file(char *filename)
+{
+    mapped_file_t mapped_file;
+    FILE *fp = fopen(filename, READONLY);
+    if (fp == NULL)
+    {
+        error("Cannot open file.");
+    }
+    int fd = fileno(fp);
+    mapped_file.size = file_size(fd);
+    mapped_file.addr = mmap(NULL, mapped_file.size, PROT_READ, MAP_SHARED, fd, 0);
+    return mapped_file;
+}
+
+size_t file_size(int fd)
+{
+    struct stat statbuf;
+    if (fstat(fd, &statbuf) == -1)
+    {
+        system_error("Error reading file status");
+    }
+    return (size_t) statbuf.st_size;
 }
 
 void *search_tff(void *array_start, const long array_end, const void *to_find, long occurrence)
