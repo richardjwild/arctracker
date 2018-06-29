5e4365b8198459a4cb9b8cf8c2b7d82e90067f79
Richard Wild <richardwild76@gmail.com>
2018-06-29 08:07:02 +0100

    Replace all calls to read_nchar with strncpy and memcpy

--- read_mod.c
+++ read_mod.c
@@ -32,12 +32,6 @@ void *search_tff2(
 #define CHUNK_NOT_FOUND_2 NULL
 #define SAMPLE_INVALID NULL
 
-void read_nchar(
-        char *p_output,
-        void *p_input,
-        int p_num_chars,
-        bool p_null_term);
-
 void get_patterns(void *p_search_from, long p_array_end, void **p_patterns);
 
 int get_samples(void *array_start, long array_end, sample_t *samples);
@@ -115,16 +109,17 @@ module_t read_tracker_file(mapped_file_t file)
 {
 	void *chunk_address;
 	long array_end = (long) file.addr + file.size;
-    module_t module = {
-            .format = TRACKER,
-            .initial_speed = 6,
-            .samples = allocate_array(36, sizeof(sample_t))
-    };
+    module_t module;
+
+    memset(&module, 0, sizeof(module_t));
+    module.format = TRACKER;
+    module.initial_speed = 6;
+    module.samples = allocate_array(36, sizeof(sample_t));
 
 	if (search_tff(file.addr, &chunk_address, array_end, TINF_CHUNK, 1) == CHUNK_NOT_FOUND)
 		error("Modfile corrupt - TINF chunk not found");
 
-    read_nchar(module.tracker_version, chunk_address + 8, 4, true);
+    strncpy(module.tracker_version, chunk_address + 8, 4);
 
     if (search_tff(file.addr, &chunk_address, array_end, MVOX_CHUNK, 1) == CHUNK_NOT_FOUND)
 		error("Modfile corrupt - MVOX chunk not found");
@@ -134,17 +129,17 @@ module_t read_tracker_file(mapped_file_t file)
     if (search_tff(file.addr, &chunk_address, array_end, STER_CHUNK, 1) == CHUNK_NOT_FOUND)
 		error("Modfile corrupt - STER chunk not found");
 
-    read_nchar(module.default_channel_stereo, chunk_address + 8, MAX_CHANNELS, false);
+    memcpy(module.default_channel_stereo, chunk_address + 8, MAX_CHANNELS);
 
     if (search_tff(file.addr, &chunk_address, array_end, MNAM_CHUNK, 1) == CHUNK_NOT_FOUND)
 		error("Modfile corrupt - MNAM chunk not found");
 
-    read_nchar(module.name, chunk_address + 8, MAX_LEN_TUNENAME, true);
+    strncpy(module.name, chunk_address + 8, MAX_LEN_TUNENAME);
 
     if (search_tff(file.addr, &chunk_address, array_end, ANAM_CHUNK, 1) == CHUNK_NOT_FOUND)
 		error("Modfile corrupt - ANAM chunk not found");
 
-    read_nchar(module.author, chunk_address + 8, MAX_LEN_AUTHOR, true);
+    strncpy(module.author, chunk_address + 8, MAX_LEN_AUTHOR);
 
     if (search_tff(file.addr, &chunk_address, array_end, MLEN_CHUNK, 1) == CHUNK_NOT_FOUND)
 		error("Modfile corrupt - MLEN chunk not found");
@@ -159,12 +154,12 @@ module_t read_tracker_file(mapped_file_t file)
     if (search_tff(file.addr, &chunk_address, array_end, PLEN_CHUNK, 1) == CHUNK_NOT_FOUND)
 		error("Modfile corrupt - PLEN chunk not found");
 
-    read_nchar(module.pattern_length, chunk_address + 8, NUM_PATTERNS, false);
+    memcpy(module.pattern_length, chunk_address + 8, NUM_PATTERNS);
 
     if (search_tff(file.addr, &chunk_address, array_end, SEQU_CHUNK, 1) == CHUNK_NOT_FOUND)
 		error("Modfile corrupt - SEQU chunk not found");
 
-    read_nchar(module.sequence, chunk_address + 8, MAX_TUNELENGTH, false);
+    memcpy(module.sequence, chunk_address + 8, MAX_TUNELENGTH);
 
     get_patterns(file.addr, array_end, module.patterns);
 	module.num_samples = get_samples(file.addr, array_end, module.samples);
@@ -177,20 +172,21 @@ module_t read_desktop_tracker_file(mapped_file_t file)
 	void *tmp_ptr;
 	long foo;
 	int i;
-    module_t module = {
-            .format = DESKTOP_TRACKER,
-            .initial_speed = 6
-    };
+    module_t module;
 
-	read_nchar(module.name, file.addr+4, MAX_LEN_TUNENAME_DSKT, true);
+    memset(&module, 0, sizeof(module_t));
+    module.format = DESKTOP_TRACKER;
+    module.initial_speed = 6;
 
-	read_nchar(module.author, file.addr+68, MAX_LEN_AUTHOR_DSKT, true);
+	strncpy(module.name, file.addr+4, MAX_LEN_TUNENAME_DSKT);
+
+	strncpy(module.author, file.addr+68, MAX_LEN_AUTHOR_DSKT);
 
 	memcpy(&module.num_channels, file.addr+136, 4);
 
 	memcpy(&module.tune_length, file.addr+140, 4);
 
-	read_nchar(module.default_channel_stereo, file.addr+144, MAX_CHANNELS_DSKT, false);
+	memcpy(module.default_channel_stereo, file.addr+144, MAX_CHANNELS_DSKT);
 
 	memcpy(&module.initial_speed, file.addr+152, 4);
 
@@ -198,7 +194,7 @@ module_t read_desktop_tracker_file(mapped_file_t file)
 
 	memcpy(&module.num_samples, file.addr+164, 4);
 
-	read_nchar(module.sequence, file.addr+168, module.tune_length, false);
+	memcpy(module.sequence, file.addr+168, (size_t) module.tune_length);
 
 	tmp_ptr = file.addr + 168 + (((module.tune_length + 3)>>2)<<2); /* align to word boundary */
 	for (i=0; i<module.num_patterns; i++)
@@ -218,6 +214,7 @@ module_t read_desktop_tracker_file(mapped_file_t file)
 		tmp_ptr = tmp_ptr + (4 - (module.num_patterns % 4));
 
 	module.samples = allocate_array(module.num_samples, sizeof(sample_t));
+	memset(module.samples, 0, sizeof(sample_t) * module.num_samples);
 	for (i=0; i<module.num_samples; i++)
 	{
         module.samples[i].transpose = 26 - *(unsigned char *)tmp_ptr++;
@@ -236,7 +233,7 @@ module_t read_desktop_tracker_file(mapped_file_t file)
 		tmp_ptr+=4;
 		memcpy(&(module.samples[i].sample_length), tmp_ptr, 4);
 		tmp_ptr+=4;
-		read_nchar(module.samples[i].name, tmp_ptr, MAX_LEN_SAMPLENAME_DSKT, true);
+		strncpy(module.samples[i].name, tmp_ptr, MAX_LEN_SAMPLENAME_DSKT);
 		tmp_ptr+=MAX_LEN_SAMPLENAME_DSKT;
 		memcpy(&foo, tmp_ptr, 4);
         module.samples[i].sample_data = file.addr + foo;
@@ -292,25 +289,6 @@ void *search_tff2(
 		array_start += 1;
 	}
 	return CHUNK_NOT_FOUND_2;
-}
-
-/* function read_nchar.                                                                               *
- * Reads n characters at the address pointed to by p_input and returns it as a null-terminated string *
- * pointed at by p_output.  p_output must point at an array large enough to hold n+1 chars!           */
-
-void read_nchar(
-	char *p_output,
-	void *p_input,
-	int p_num_chars,
-	bool p_null_term)
-{
-	int i;
-
-	for (i=0; i<p_num_chars; i++)
-		p_output[i] = *(((char *)p_input) + i);
-
-	if (p_null_term)
-		p_output[p_num_chars] = '\0';
 }
 
 void get_patterns(void *p_search_from, long p_array_end, void **p_patterns)
