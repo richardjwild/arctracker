868fc5ce9b47e7e13566eb4f0f088388d9145eec
Richard Wild <richardwild76@gmail.com>
2018-07-07 20:23:29 +0100

    Bit of formatting and correct macro names

--- console.c
+++ console.c
@@ -6,17 +6,23 @@ static int pianola_tracks;
 static int tune_length;
 
 static const
-char *notes[] = {"---",
-                 "C-1", "C#1", "D-1", "D#1", "E-1", "F-1", "F#1", "G-1", "G#1", "A-1", "A#1", "B-1",
-                 "C-2", "C#2", "D-2", "D#2", "E-2", "F-2", "F#2", "G-2", "G#2", "A-2", "A#2", "B-2",
-                 "C-3", "C#3", "D-3", "D#3", "E-3", "F-3", "F#3", "G-3", "G#3", "A-3", "A#3", "B-3"};
+char *notes[] =
+        {
+                "---",
+                "C-1", "C#1", "D-1", "D#1", "E-1", "F-1", "F#1", "G-1", "G#1", "A-1", "A#1", "B-1",
+                "C-2", "C#2", "D-2", "D#2", "E-2", "F-2", "F#2", "G-2", "G#2", "A-2", "A#2", "B-2",
+                "C-3", "C#3", "D-3", "D#3", "E-3", "F-3", "F#3", "G-3", "G#3", "A-3", "A#3", "B-3"
+        };
 
 static const
-char alphanum[] = {'-',
-                   '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-                   'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-                   'U', 'V', 'W', 'X', 'Y', 'Z'};
+char alphanum[] =
+        {
+                '-',
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                'U', 'V', 'W', 'X', 'Y', 'Z'
+        };
 
 void configure_console(const bool pianola, const module_t *module)
 {
@@ -32,10 +38,10 @@ void configure_console(const bool pianola, const module_t *module)
 
 void output_new_position(const positions_t *positions)
 {
-    if (!pianola_mode) {
+    if (!pianola_mode)
+    {
         printf(
-                "%cPlaying position %d of %d ",
-                13,
+                "\rPlaying position %d of %d ",
                 positions->position_in_sequence + 1,
                 tune_length);
         fflush(stdout);
--- console.h
+++ console.h
@@ -1,5 +1,5 @@
-#ifndef ARCTRACKER_PIANOLA_H
-#define ARCTRACKER_PIANOLA_H
+#ifndef ARCTRACKER_CONSOLE_H
+#define ARCTRACKER_CONSOLE_H
 
 void configure_console(const bool pianola, const module_t *module);
 
@@ -7,4 +7,4 @@ void output_new_position(const positions_t *positions);
 
 void pianola_roll(const positions_t *positions, const channel_event_t *line);
 
-#endif //ARCTRACKER_PIANOLA_H
+#endif //ARCTRACKER_CONSOLE_H
