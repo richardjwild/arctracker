07d6e87c0ad2fad012f6b1ff40cbb3c1261bf9e0
Richard Wild <richardwild76@gmail.com>
2018-07-20 07:29:59 +0100

    Move process_commands into new module effects.c

---
+++ effects.h
@@ -0,0 +1,9 @@
+#ifndef ARCTRACKER_EFFECTS_H
+#define ARCTRACKER_EFFECTS_H
+
+#include "arctracker.h"
+#include "play_mod.h"
+
+void process_commands(channel_event_t *event, voice_t *voice, bool on_event);
+
+#endif //ARCTRACKER_EFFECTS_H
