76a79e6bdafdea6ff289f8476400ba266621dd81
Richard Wild <richardwild76@gmail.com>
2018-08-15 11:44:52 +0100

    Autoformat

--- src/audio/resample.h
+++ src/audio/resample.h
@@ -7,6 +7,6 @@ void calculate_phase_increments(long p_sample_rate);
 
 void allocate_resample_buffer(int no_of_frames);
 
-float *resample(voice_t* voice, long frames_to_write);
+float *resample(voice_t *voice, long frames_to_write);
 
 #endif // ARCTRACKER_RESAMPLE_H
