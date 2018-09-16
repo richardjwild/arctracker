f030be768d0c6cf24c3806c3b0025b6bdd700ed0
Richard Wild <richardwild76@gmail.com>
2018-09-16 10:34:33 +0100

    Fallback to WAV when no audio output available

--- src/audio_api/api.c
+++ src/audio_api/api.c
@@ -1,32 +1,54 @@
 #include <config.h>
-#include "api_wav.h"
+#include "api.h"
 
 #if HAVE_LIBASOUND
 
-#include "api.h"
 #include "api_alsa.h"
+#include "api_wav.h"
 
 audio_api_t initialise_audio_api(args_t config)
 {
-    return config.output_filename ? initialise_wav(config.output_filename) : initialise_alsa();
+    return config.output_filename
+            ? initialise_wav(config.output_filename)
+            : initialise_alsa();
 }
 
-
 #elif HAVE_LIBPORTAUDIO
+
 #include "api_portaudio.h"
+#include "api_wav.h"
 
 audio_api_t initialise_audio_api(args_t config)
 {
-    return config.output_filename ? initialise_wav(config.output_filename) : initialise_portaudio();
+    return config.output_filename
+            ? initialise_wav(config.output_filename)
+            : initialise_portaudio();
 }
 
 #elif HAVE_SYS_SOUNDCARD_H
 
 #include "api_oss.h"
+#include "api_wav.h"
 
 audio_api_t initialise_audio_api(args_t config)
 {
-    return config.output_filename ? initialise_wav(config.output_filename) : initialise_oss();
+    return config.output_filename
+            ? initialise_wav(config.output_filename)
+            : initialise_oss();
+}
+
+#else
+
+#include "api_wav.h"
+#include <io/error.h>
+
+audio_api_t initialise_audio_api(args_t config)
+{
+    if (!config.output_filename)
+    {
+        error("No audio system support available. WAV file only.");
+    }
+    return initialise_wav(config.output_filename);
 }
 
 #endif
