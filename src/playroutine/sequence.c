d1702aeb9f73bcbab4f85dbb90fb41ae67b6dbc8
Richard Wild <richardwild76@gmail.com>
2018-10-20 21:32:41 +0100

    Jump to position 0 was crashing on macos - fixed

--- src/playroutine/sequence.c
+++ src/playroutine/sequence.c
@@ -1,6 +1,9 @@
 #include "sequence.h"
 
+static const int NO_JUMP = -1;
+
 static int position_in_sequence;
+static int jump_position_target;
 static int position_in_pattern;
 static void *pattern_line_ptr;
 static const int *sequence;
@@ -14,6 +17,7 @@ void initialise_sequence(module_t *module)
 {
     position_in_sequence = 0;
     position_in_pattern = -1;
+    jump_position_target = NO_JUMP;
     sequence = module->sequence;
     tune_length = module->tune_length;
     patterns = module->patterns;
@@ -39,23 +43,58 @@ int pattern_position()
     return position_in_pattern;
 }
 
-void next_event()
+void begin_new_pattern()
+{
+    int next_pattern = sequence[position_in_sequence];
+    pattern_line_ptr = patterns[next_pattern];
+    position_in_pattern = 0;
+}
+
+static inline
+bool end_of_sequence()
+{
+    return (position_in_sequence == tune_length);
+}
+
+void advance_sequence_position()
+{
+    position_in_sequence += 1;
+    if (end_of_sequence())
+    {
+        position_in_sequence = 0;
+        looped = true;
+    }
+    begin_new_pattern();
+}
+
+static inline
+bool end_of_pattern()
 {
     int current_pattern = sequence[position_in_sequence];
     int pattern_length = pattern_lengths[current_pattern];
+    return (position_in_pattern == pattern_length);
+}
+
+void advance_pattern_event()
+{
     position_in_pattern += 1;
-    if (position_in_pattern == pattern_length)
-    {
-        position_in_sequence += 1;
-        position_in_pattern = 0;
-        if (position_in_sequence == tune_length)
-        {
-            position_in_sequence = 0;
-            looped = true;
-        }
-        int next_pattern = sequence[position_in_sequence];
-        pattern_line_ptr = patterns[next_pattern];
-    }
+    if (end_of_pattern())
+        advance_sequence_position();
+}
+
+void go_to_jump_target()
+{
+    position_in_sequence = jump_position_target;
+    jump_position_target = NO_JUMP;
+    begin_new_pattern();
+}
+
+void next_event()
+{
+    if (jump_position_target == NO_JUMP)
+        advance_pattern_event();
+    else
+        go_to_jump_target();
 }
 
 void *pattern_line()
@@ -73,12 +112,6 @@ bool looped_yet()
     return looped;
 }
 
-void break_to_next_position()
-{
-    int current_pattern = sequence[position_in_sequence];
-    position_in_pattern = pattern_lengths[current_pattern] - 1;
-}
-
 static inline
 bool jump_permitted(int next_position)
 {
@@ -89,9 +122,10 @@ bool jump_permitted(int next_position)
 void jump_to_position(int next_position)
 {
     if (jump_permitted(next_position))
-    {
-        position_in_sequence = next_position - 1;
-        int current_pattern = sequence[position_in_sequence];
-        position_in_pattern = pattern_lengths[current_pattern] - 1;
-    }
+        jump_position_target = next_position;
+}
+
+void break_to_next_position()
+{
+    jump_to_position(position_in_sequence + 1);
 }
