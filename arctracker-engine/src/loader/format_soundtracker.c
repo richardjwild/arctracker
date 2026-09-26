30bd7e9fef219ce0aca4756696bb2b6bdc9aa14a
Richard Wild <richardwild76@gmail.com>
2026-09-26 17:39:39 +0100

    Remove unused import

--- arctracker-engine/src/loader/format_soundtracker.c
+++ arctracker-engine/src/loader/format_soundtracker.c
@@ -5,7 +5,6 @@
 #include <stdio.h>
 #include <string.h>
 #include "memory/heap.h"
-#include "src/vidc/vidc.h"
 
 /*
  * Documentation lifted from Andrew Scott's website: https://www.aes.id.au/modformat.html
